cae8d9d2852455fe67211f2bc50fd572191ceafc
edwingpe <edwing.machado1@gmail.com>
2023-01-09 02:05:42 -0300

    adding
    Models & Repository

--- backend/src/main/java/com/createPortfolio/backend/repository/RolRepository.java
+++ backend/src/main/java/com/createPortfolio/backend/repository/RolRepository.java
@@ -4,10 +4,19 @@
  */
 package com.createPortfolio.backend.repository;
 
+import com.createPortfolio.backend.models.ERole;
+import com.createPortfolio.backend.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
 /**
  *
  * @author edwin
  */
-public class RolRepository {
+@Repository
+public interface RolRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
     
 }
--- backend/src/main/java/com/createPortfolio/backend/repository/UserRepository.java
+++ backend/src/main/java/com/createPortfolio/backend/repository/UserRepository.java
@@ -4,10 +4,20 @@
  */
 package com.createPortfolio.backend.repository;
 
+import com.createPortfolio.backend.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
 /**
  *
  * @author edwin
  */
-public class UserRepository {
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+    Optional<User> findByUsername(String username);
+    Boolean existByUsername(String username);
+    Boolean existByEmail(String email);
     
 }
